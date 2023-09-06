332bb35451c2ee3ef66c6895fe314f0c133ebf5d
Ehor <ehorlukianov@gmail.com>
2023-09-06 13:38:21 +0300

    add tours component

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+    const [readMore, setReadMore] = useState(false);
     return ( <
         article className = "single-tour" >
         <
@@ -15,7 +16,11 @@ const Tour = ({ id, image, info, price, name }) => {
         h4 className = "tour-price" > { price }
         $ < /h4> <
         /div> <
-        p > { info } < /p> <
+        p > { readMore ? info : `${info.substring(0, 200)}...` } <
+        button onClick = {
+            () => setReadMore(!readMore) } > { readMore ? "Show less" : "Show more" } <
+        /button> <
+        /p> <
         /footer> <
         button className = "delete-btn" > not interested < /button> <
         /article>
