501d0602f3adcc3887a621cf38bea6e48f291de9
Ehor <ehorlukianov@gmail.com>
2023-09-06 13:45:08 +0300

    add refresh page

--- src/App.js
+++ src/App.js
@@ -43,8 +43,25 @@ function App() {
             /main>
         );
     }
+    if (tours.length === 0) {
+        return ( <
+            main >
+            <
+            div className = "title" >
+            <
+            h2 > no tours left < /h2> <
+            button className = "btn"
+            onClick = { fetchTours } >
+            Refresh <
+            /button> <
+            /div> <
+            /main>
+        );
+    }
 
-    return ( <
+    return (
+
+        <
         main >
         <
         Tours removeTour = { removeTour }
